d2ec34fb6e2679b9ac7d17e2acc0956227db7cc1
Chairenerose <76175773+Chairenerose@users.noreply.github.com>
2021-01-01 23:44:08 +0100

    Attempt at adding relevant icon

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
-function showTemperature(response) {
+let now = new Date();
 
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[now.getDay()];
+let currentHour = now.getHours();
+let currentMinutes = now.getMinutes();
+
+
+let date = document.querySelector("#date");
+date.innerHTML = `${day}`;
+
+let time = document.querySelector("#time");
+time.innerHTML = `${currentHour}:${currentMinutes}`;
+
+
+
+
+function showTemperature(response) {
+    console.log(response.data);
     let temperature = document.querySelector("#degrees");
     let temperatureResponse = Math.round(response.data.main.temp);
     temperature.innerHTML = `${temperatureResponse}°C`;
@@ -13,10 +30,17 @@ function showTemperature(response) {
     let wind = document.querySelector("#windInfo");
     let windResult = response.data.wind.speed;
     wind.innerHTML = `${windResult} km/h`;
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    console.log(response.data);
+
 }
 
+
 let apiKey = "26dddc2844f26171cf43bb8923cb9f4b";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=Dubai&appid=${apiKey}&units=metric`;
+let cityName = "Dubai";
+let metric = "metric";
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${metric}`;
 
 
 axios.get(apiUrl).then(showTemperature);
